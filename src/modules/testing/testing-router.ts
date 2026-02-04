66b765366cd04bb47c858325356a6c98b7211b11
Ivan S <xsmirx@gmail.com>
2026-02-05 01:29:34 +0700

    feat: add user repository cleanup in testing router delete endpoint

--- src/modules/testing/testing-router.ts
+++ src/modules/testing/testing-router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { blogRepository } from '../blog/blog-repository';
 import { postRepository } from '../post/post-repository';
+import { userRepository } from '../user/user-repository';
 
 export const testingRouter: Router = Router();
 
 testingRouter.delete('/', async (req, res) => {
+  await userRepository.collection.deleteMany();
   await blogRepository.getCollection().deleteMany();
   await postRepository.getCollection().deleteMany();
   res.sendStatus(204);
